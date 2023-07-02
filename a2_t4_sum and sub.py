f483277652e2d327d0fdeeeb1dafa605aa834e30
Tahahokmabadi <137491279+Tahahokmabadi@users.noreply.github.com>
2023-07-02 03:35:49 +0330

    Update and rename a2_t4_sum and sub.py to Assignment_2/a2_t4_sum and sub.py

--- a2_t4_sum and sub.py
+++
@@ -1,8 +0,0 @@
-a = input("Enter the first number: ")
-b = input("Enter the second number: ")
-a = int(a)
-b = int(b)
-x = a + b
-y = a - b
-print("The sum of these two numbers is",x)
-print("The substraction of these two numbers is",y)
