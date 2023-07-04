d294724515cfba8ea5a62271141d3265b90d88be
Tahahokmabadi <137491279+Tahahokmabadi@users.noreply.github.com>
2023-07-04 11:50:47 +0330

    Add files via upload

---
+++ t1_ceil&floor.py
@@ -0,0 +1,10 @@
+import math
+
+x = float(input("Enter an number: "))
+
+
+# math.ceil(x) shows the smallest integer greater than or equal to x.
+print("Rounded to ceil:",math.ceil(x))
+
+# math.floor(x) shows the smallest integer not greater than or equal to x.
+print("Rounded to floor:",math.floor(x))
