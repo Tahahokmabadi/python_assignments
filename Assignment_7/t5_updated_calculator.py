17efc719fd94f6ff4cc67c81b74d73e82bc937f1
Taha <137491279+Tahahokmabadi@users.noreply.github.com>
2023-07-14 23:52:25 +0330

    Update t5_updated_calculator.py

--- Assignment_7/t5_updated_calculator.py
+++ Assignment_7/t5_updated_calculator.py
@@ -1,105 +1,97 @@
 import math
-
 status = True
-c = 0
-exit = ""
-sum = 0
-number_a = float(input("Enter a number : "))
-number_b = 0
-breaker = False
-while True :
-        if breaker == True :
-            break
-        print("1. +")
-        print("2. -")
-        print("3. *")
-        print("4. /")
-        print("5. //")
-        print("6. %")
-        print("7. **")
-        print("8. jazr")
-        print("9. log")
-        print("10. sin")
-        print("11. tan")
-        print("12. cos")
-        print("13. factorial")
-        print("14. abs")
-        print("15. Round")
+number_a = float(input("Enter the first number : "))
 
-        operator = input("Enter a number for the operator from the list above: ")
-        operator = int(operator)
-    
-        if operator == ( 8 and 9 and 10 and 11 and 12 and 13 and 14 and 15 ):
-            print("(second number is not allowed for this operator)")
+while status == True:
+
+    print("1. +")
+    print("2. -")
+    print("3. *")
+    print("4. /")
+    print("5. //")
+    print("6. %")
+    print("7. **")
+    print("8. jazr")
+    print("9. log")
+    print("10. sin")
+    print("11. tan")
+    print("12. cos")
+    print("13. factorial")
+    print("14. abs")
+    print("15. Round")
+
+    operator = int(input("Enter a number for the operator from the list above: "))
+
+    number_b = float(input("Enter a number : "))
+    if number_b == 0 and (operator == 3 or operator == 5 or operator == 6):
+            print("Can't divide by zero")
+    else :
+        if operator == 1 :
+                        number_a = (number_a + number_b)
+                        print("total: ", number_a)
+                        exit = ""
+        elif operator == 2 :
+                        number_a = (number_a - number_b)
+                        print("total: ", number_a)
+                        exit = ""
+        elif operator == 3 :
+                        number_a = (number_a * number_b)
+                        print("total: ", number_a)
+                        exit = ""
+        elif operator == 4 :
+                        number_a = (number_a / number_b)
+                        print("total: ", number_a)
+                        exit = ""
+        elif operator == 5 :
+                        number_a = (number_a // number_b)
+                        print("total: ", number_a)
+                        exit = ""
+        elif operator == 6 :
+                        number_a = (number_a % number_b)
+                        print("total: ", number_a)
+                        exit = ""
+        elif operator == 7 :
+                        number_a = (number_a ** number_b)
+                        print("total: ", number_a)
+                        exit = ""
+        elif operator == 8 :
+                    number_a = (math.sqrt(number_a))
+                    print("total: ", number_a)
+                    exit = ""
+        elif operator == 9 :
+                        number_a = (math.log(number_a))
+                        print("total: ", number_a)
+                        exit = ""
+        elif operator == 10 :
+                        number_a = (math.sin(math.radians(number_a)))
+                        print("total: ", number_a)
+                        exit = ""
+        elif operator == 11 :
+                        number_a = (math.tan(number_a))
+                        print("total: ", number_a)
+                        exit = ""
+        elif operator == 12 :
+                        number_a = (math.cos(number_a))
+                        print("total: ", number_a)
+                        exit = ""
+        elif operator == 13 :
+                        number_a = (math.factorial(number_a))
+                        print("total: ", number_a)
+                        exit = ""
+        elif operator == 14 :
+                        number_a = (abs(number_a))
+                        print("total: ", number_a)
+                        exit = ""
+        elif operator == 15 :
+                        number_a = (round(number_a))
+                        print("total: ", number_a)
+                        exit = ""
         else :
-            number_b = float(input("Enter a number : "))
-
-        if number_b == 0 and (operator == (4 or 5 or 6)):
-                print("Can't divide by zero")
-        else :
-            if operator == 1 :
-                    number_a = (number_a + number_b)
-                    print("total: ", number_a)
-                    exit = ""
-            elif operator == 2 :
-                    number_a = (number_a - number_b)
-                    print("total: ", number_a)
-                    exit = ""
-            elif operator == 3 :
-                    number_a = (number_a * number_b)
-                    print("total: ", number_a)
-                    exit = ""
-            elif operator == 4 :
-                    number_a = (number_a / number_b)
-                    print("total: ", number_a)
-                    exit = ""
-            elif operator == 5 :
-                    number_a = (number_a // number_b)
-                    print("total: ", number_a)
-                    exit = ""
-            elif operator == 6 :
-                    number_a = (number_a % number_b)
-                    print("total: ", number_a)
-                    exit = ""
-            elif operator == 7 :
-                    number_a = (number_a ** number_b)
-                    print("total: ", number_a)
-                    exit = ""
-            elif operator == 8 :
-                   number_a = (math.sqrt(number_a))
-                   print("total: ", number_a)
-                   exit = ""
-            elif operator == 9 :
-                    number_a = (math.log(number_a))
-                    print("total: ", number_a)
-                    exit = ""
-            elif operator == 10 :
-                    number_a = (math.sin(math.radians(number_a)))
-                    print("total: ", number_a)
-                    exit = ""
-            elif operator == 11 :
-                    number_a = (math.tan(number_a))
-                    print("total: ", number_a)
-                    exit = ""
-            elif operator == 12 :
-                    number_a = (math.cos(number_a))
-                    print("total: ", number_a)
-                    exit = ""
-            elif operator == 13 :
-                    number_a = (math.factorial(number_a))
-                    print("total: ", number_a)
-                    exit = ""
-            elif operator == 14 :
-                    number_a = (abs(number_a))
-                    print("total: ", number_a)
-                    exit = ""
-            elif operator == 15 :
-                    number_a = (round(number_a))
-                    print("total: ", number_a)
-                    exit = ""
-            else :
-                    print("Only numbers from the list are acceptable as the operator, please try again.")
-    
-
-
-print("total:", number_a)
+                        print("Only numbers from the list are acceptable as the operator, please try again.")
+        Ask = input("Do you want to continue?(yes or no) ")
+        if Ask == "yes":
+            print("ok")
+        elif Ask == "no":
+            status = False
+        else:
+            print("only yes or no are accepted. try again.")
