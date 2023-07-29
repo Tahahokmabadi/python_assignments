e5153db0b5f85440b26e4bf7489f6acb89aaacb6
Taha <137491279+Tahahokmabadi@users.noreply.github.com>
2023-07-29 23:52:53 +0330

    Update and rename class_teacher_student_course.py to Assignment_14/class_teacher_student_course.py

---
+++ Assignment_14/class_teacher_student_course.py
@@ -0,0 +1,34 @@
+class Teacher:
+      # Properties
+      firstname = None
+      lastname  None
+      age = None
+      nationality = None
+      gender = None
+      proffesion = None
+      # Methods
+      def teach_lessons(): pass
+      def teke_exams(): pass
+      def practice_lessons(): pass
+class Student:
+      # Properties
+      firstname = None
+      lastname = None
+      age = None
+      gender = None
+      f = None
+      score = None
+      # Methods
+      def learn(): pass
+      def study(): pass
+      def do_homework(): pass
+      def hang_out_with_friend(): pass
+class Course:
+      # Properties
+      member = None
+      name = None
+      time = None
+      number_of_students = None
+      # Methods
+      def increase_knowledge(): pass
+      def becoming_more_skilled(): pass
