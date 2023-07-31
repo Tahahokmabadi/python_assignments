0f1173c21d3e5092ab1b5152d00ef49039731dc4
Taha <137491279+Tahahokmabadi@users.noreply.github.com>
2023-07-31 05:15:55 +0330

    Update t1-upper_and_lower_counter.py

---
+++ Assignment_12/t1-upper_and_lower_counter.py
@@ -0,0 +1,16 @@
+def counter(string):
+    upper_count = 0
+    lower_count = 0
+
+    for char in string:
+        if 90 >= ord(char) >= 65:
+            upper_count += 1
+        elif 122 >= (ord(char)) >= 97 :
+            lower_count += 1
+
+    return upper_count, lower_count
+
+input = input("Enter a string: ")
+upper, lower = counter((_((input)
+print("uppercases: ", upper)
+print("lowercases: ", lower
